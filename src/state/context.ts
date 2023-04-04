812c961a57f811de8059098a22005b07654b04dc
Carmelo Pullara <carmelo@pullara.me>
2023-04-04 17:13:39 +0200

    Add a default value for context

--- src/state/context.ts
+++ src/state/context.ts
@@ -3,6 +3,7 @@ import { createContext } from 'react'
 // ShortenedUrl interface represents the structure of a single shortened URL object.
 export interface ShortenedUrl {
   code: string
+  fullShortLink: string
   shortLink: string
   originalLink: string
   timestamp: number
@@ -12,9 +13,26 @@ export interface ShortenedUrl {
  * ShortenedUrlContextType interface represents the structure of the context value,
  * containing the list of shortened URLs and the function to add a new shortened URL.
  */
-interface ShortenedUrlContextType {
+export interface ShortenedUrlContextType {
   shortenedUrls: ShortenedUrl[]
   addShortenedUrl: (url: ShortenedUrl) => void
+}
+
+// A default no-op function for addShortenedUrl that logs a warning
+// when it's called without a valid UrlDataProvider in the component tree.
+const defaultAddShortenedUrl = (newUrl: ShortenedUrl) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn(
+      'Warning: addShortenedUrl() was called without a valid UrlDataProvider in the component tree.',
+      'Please ensure that your component is wrapped by a UrlDataProvider.'
+    )
+  }
+}
+
+// Define the default value for UrlDataContext based on UrlDataContextValue
+const defaultState: ShortenedUrlContextType = {
+  shortenedUrls: [],
+  addShortenedUrl: defaultAddShortenedUrl,
 }
 
 /**
@@ -22,4 +40,4 @@ interface ShortenedUrlContextType {
  * It has a value of type ShortenedUrlContextType or null.
  */
 export const ShortenedUrlContext =
-  createContext<ShortenedUrlContextType | null>(null)
+  createContext<ShortenedUrlContextType>(defaultState)
