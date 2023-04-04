3f1a36eb27eb3d21b3cec24e86be497fbb09eb9b
Carmelo Pullara <carmelo@pullara.me>
2023-04-04 17:20:56 +0200

    Update jest configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['jest-fetch-mock'],
 }
