295895e21c5308fdfcbc986ad8a3588d11336343
Carmelo Pullara <carmelo@pullara.me>
2023-04-05 10:32:43 +0200

    Improve types on useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -8,7 +8,7 @@ import { useState, useEffect, Dispatch, SetStateAction } from 'react'
  * @returns {[T, Dispatch<SetStateAction<T>>]} A tuple with the state value and a setter function.
  */
 
-export function useLocalStorage<T extends any>(
+export function useLocalStorage<T>(
   key: string,
   initialValue: T
 ): [T, Dispatch<SetStateAction<T>>] {
